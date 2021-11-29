891af29caeb24ebb9110c718c083a1060d6aa98b
AlexeyKolesnikoff <92367003+AlexeyKolesnikoff@users.noreply.github.com>
2021-11-29 23:30:55 +0300

    Delete PZ_5_1.py

--- PZ_5_1.py
+++
@@ -1,8 +0,0 @@
-# Составить функцию, которая напечатает 40 любых символов.
-# Вариант 20
-
-def generate_random_string(num):
-    x = input("Введите случайный символ: ")
-    print(str(x) * num)
-
-generate_random_string(40)
